49e8d5261af27e3f0e4d6cb26d157094decf4860
haodd0216 <1903289197@qq.com>
2025-08-15 18:16:19 +0800

    fix: 防抖配置增加校验(#35886)

--- views/Scene/Save/components/ShakeLimit/index.vue
+++ views/Scene/Save/components/ShakeLimit/index.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="shakeLimit">
-        <a-switch
-            :checkedChildren="$t('ShakeLimit.index.9093426-0')"
-            :unCheckedChildren="$t('ShakeLimit.index.9093426-1')"
-            v-model:checked="shakeLimit.enabled"
-            style="margin-right: 12px"
-        />
-        <template v-if="shakeLimit.enabled">
-            <a-input-number :min="1"  :precision="0" size="small" v-model:value="shakeLimit.time" style="width: 38px" />
-            <span>{{ $t('ShakeLimit.index.9093426-2') }}</span>
-            <a-input-number :min="1" :max="100" :precision="0" size="small" v-model:value="shakeLimit.threshold" style="width: 38px" />
-            <span>{{ $t('ShakeLimit.index.9093426-3') }}</span>
-            <a-radio-group :options="alarmFirstOptions" optionType="button" v-model:value="shakeLimit.alarmFirst" size="small" @change="handleChange"/>
-        </template>
+        <a-form-item name="shakeLimit" style="margin-bottom: 0;" :rules="{
+          validator,
+          trigger: 'change'
+        }">
+          <a-switch
+              :checkedChildren="$t('ShakeLimit.index.9093426-0')"
+              :unCheckedChildren="$t('ShakeLimit.index.9093426-1')"
+              v-model:checked="shakeLimit.enabled"
+              style="margin-right: 12px"
+              @change="handleCheckedChange"
+          />
+          <template v-if="shakeLimit.enabled">
+              <a-input-number :min="1"  :precision="0" size="small" v-model:value="shakeLimit.time" style="width: 38px" />
+              <span>{{ $t('ShakeLimit.index.9093426-2') }}</span>
+              <a-input-number :min="1" :max="100" :precision="0" size="small" v-model:value="shakeLimit.threshold" style="width: 38px" />
+              <span>{{ $t('ShakeLimit.index.9093426-3') }}</span>
+              <a-radio-group :options="alarmFirstOptions" optionType="button" v-model:value="shakeLimit.alarmFirst" size="small" @change="handleChange"/>
+          </template>
+        </a-form-item>
     </div>
 </template>
 
@@ -55,10 +61,29 @@ const shakeLimit = reactive<ShakeLimitType>({
     outputFirst: undefined
 })
 
+const validator = (rule: any, value: ShakeLimitType) => {
+  if (shakeLimit.enabled) {
+    if (!shakeLimit.time || !shakeLimit.threshold) {
+      return Promise.reject(new Error($t('请配置防抖')))
+    } else {
+      return Promise.resolve()
+    }
+  } else {
+    return Promise.resolve()
+  }
+}
+
 Object.assign(shakeLimit, props.value)
 
 const handleChange = (e: InputEvent) => {
   shakeLimit.outputFirst = e.target?.value
+}
+
+const handleCheckedChange = (checked: boolean) => {
+  if (!checked) {
+    shakeLimit.time = shakeLimit.time || 1
+    shakeLimit.threshold = shakeLimit.time || 1
+  }
 }
 
 watch(() => shakeLimit, () => {
