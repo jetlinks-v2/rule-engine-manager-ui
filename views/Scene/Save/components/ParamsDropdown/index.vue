52838c04c7a7d66cb4fb6d3240ebc9f9d5a66459
XieYongHong <18010623010@163.com>
2025-10-21 14:53:56 +0800

    fix: visible修改为open (#85)

--- views/Scene/Save/components/ParamsDropdown/index.vue
+++ views/Scene/Save/components/ParamsDropdown/index.vue
@@ -3,7 +3,7 @@
     class="scene-select-value"
     trigger="click"
     v-model:open="visible"
-    @visibleChange="visibleChange"
+    @openChange="visibleChange"
   >
     <div @click.prevent="visible = true">
       <slot :label="label">
