05a75bd7709276e3b2225606dbe2ee8164858c93
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2025-10-22 15:32:56 +0800

    fix: 当没有数据采集模块时，系统菜单展示需优化 (#89)
    * fix: 当没有数据采集模块时，系统菜单展示需优化

--- hook/useAlarmConfigType.ts
+++ hook/useAlarmConfigType.ts
@@ -1,16 +1,24 @@
 import { getTargetTypes } from '../api/configuration';
+import {useMenuStore} from "@/store";
 
 export const useAlarmConfigType = (filter: string[] = []) => {
   type Options = { label: string; value: string };
   const supports = ref<Options[]>([]);
+
+  const menuStore = useMenuStore();
+
   getTargetTypes().then((res) => {
+    const _filter = [...filter]
+    if (!menuStore.hasMenu('DataCollect/Collector')) {
+      _filter.push('collector')
+    }
     supports.value = res.result.map((item: any) => {
       return {
         label: item.name,
         value: item.id,
       };
     }).filter((item: any) => {
-      return !filter.includes(item.value)
+      return !_filter.includes(item.value)
     });
   });
   return {
