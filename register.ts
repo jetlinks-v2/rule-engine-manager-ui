f1030fb1c438d2d2dece6c25435236793af97747
sunchao <2642441182@qq.com>
2025-11-10 10:25:16 +0800

    fix: merge

--- register.ts
+++ register.ts
@@ -1,12 +1,12 @@
-import { useRulePermissionContext } from '@rule-engine-manager-ui/hook/usePermission'
+import {useRulePermissionContext} from '@rule-engine-manager-ui/hook/usePermission'
 
 export default {
-  components: {
-    ruleInstance: defineAsyncComponent(() => import('./views/Instance/index.vue')),
-      AlarmDashboard: defineAsyncComponent(() => import('./views/DashBoard/index.vue')),
-      AlarmConfig: defineAsyncComponent(() => import('./views/Alarm/Config/index.vue'))
-  },
-  hooks: {
-    useRulePermissionContext
-  }
+    components: {
+        ruleInstance: defineAsyncComponent(() => import('./views/Instance/index.vue')),
+        AlarmDashboard: defineAsyncComponent(() => import('./views/DashBoard/index.vue')),
+        AlarmConfig: defineAsyncComponent(() => import('./views/Alarm/Config/index.vue'))
+    },
+    hooks: {
+        useRulePermissionContext
+    }
 }
